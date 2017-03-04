013ed8636eb6a23d8a84700a1783fdcf3952f618
Anton Mulobaev <anton@mulobaev.ru>
2017-03-04 15:08:26 +0300

    Добавил расчёт

--- Opora/MainActivity.cs
+++ Opora/MainActivity.cs
@@ -5,6 +5,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.Globalization;
 
 namespace Opora
 {
@@ -12,6 +13,57 @@ namespace Opora
     public class MainActivity : Activity
     {
         int count = 1;
+        private Button _button;
+        private EditText _editTextH;
+        private EditText _editTextX;
+        private EditText _editTextH1;
+        private EditText _editTextH2;
+        private EditText _editTextResult;
+
+        /// <summary>
+        /// Высота опоры
+        /// </summary>
+        private EditText EditTextH
+        {
+            get { return _editTextH ?? (_editTextH = FindViewById<EditText>(Resource.Id.entry2)); }
+        }
+
+        /// <summary>
+        /// Конусность опоры
+        /// </summary>
+        private EditText EditTextX
+        {
+            get { return _editTextX ?? (_editTextX = FindViewById<EditText>(Resource.Id.entry3)); }
+        }
+
+        /// <summary>
+        /// Первое изменение
+        /// </summary>
+        private EditText EditTextH1
+        {
+            get { return _editTextH1 ?? (_editTextH1 = FindViewById<EditText>(Resource.Id.entry4)); }
+        }
+
+        /// <summary>
+        /// Второе изменение
+        /// </summary>
+        private EditText EditTextH2
+        {
+            get { return _editTextH2 ?? (_editTextH2 = FindViewById<EditText>(Resource.Id.entry5)); }
+        }
+
+        /// <summary>
+        /// Угол наклона
+        /// </summary>
+        private EditText EditTextResult
+        {
+            get { return _editTextResult ?? (_editTextResult = FindViewById<EditText>(Resource.Id.entry6)); }
+        }
+
+        private Button Button1
+        {
+            get { return _button ?? (_button = FindViewById<Button>(Resource.Id.MyButton)); }
+        }
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -22,10 +74,30 @@ namespace Opora
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            Button1.Click += ButtonClick;
+        }
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            Button1.Text = string.Format("{0} clicks!", count++);
+
+            double h, h1, h2, x;
+            if (!TryParse(EditTextH.Text, out h) || !TryParse(EditTextH1.Text, out h1) || !TryParse(EditTextH2.Text, out h2) ||
+                !TryParse(EditTextX.Text, out x))
+            {
+                //MessageBox.Show("Неверные исходные данные", Title);
+                return;
+            }
+
+            double result = x - Math.Abs(h1 - h2) * h;
+            EditTextResult.Text = result.ToString();
+        }
+
+        private bool TryParse(string s, out double result)
+        {
+            string separator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            s = s.Replace(".", separator).Replace(",", separator);
+            return double.TryParse(s, out result);
         }
     }
 }
-
--- Opora/Resources/Resource.Designer.cs
+++ Opora/Resources/Resource.Designer.cs
@@ -60,8 +60,50 @@ namespace Opora
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f05000a
+			public const int MyButton = 2131034122;
+			
+			// aapt resource value: 0x7f050001
+			public const int entry1 = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int entry2 = 2131034115;
+			
+			// aapt resource value: 0x7f050005
+			public const int entry3 = 2131034117;
+			
+			// aapt resource value: 0x7f050007
+			public const int entry4 = 2131034119;
+			
+			// aapt resource value: 0x7f050009
+			public const int entry5 = 2131034121;
+			
+			// aapt resource value: 0x7f05000c
+			public const int entry6 = 2131034124;
+			
+			// aapt resource value: 0x7f05000e
+			public const int entry7 = 2131034126;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int label1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int label2 = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int label3 = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int label4 = 2131034118;
+			
+			// aapt resource value: 0x7f050008
+			public const int label5 = 2131034120;
+			
+			// aapt resource value: 0x7f05000b
+			public const int label6 = 2131034123;
+			
+			// aapt resource value: 0x7f05000d
+			public const int label7 = 2131034125;
 			
 			static Id()
 			{
